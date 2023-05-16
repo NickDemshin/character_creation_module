ca09e3e2ffd9f08e85494cee8706c7beceeb8b42
Nikolay Demshin <NickDemshin@gmail.com>
2023-05-16 20:25:27 +0200

    Dockstrings produced by

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
-
+    """Функция нанесения урона персонажем."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -15,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция блокировки урона персонажем."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция специального уменя персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 '«Выносливость {80 + 25}»')
@@ -36,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция запуска тренировки персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -83,6 +87,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Функция запуска игры."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
